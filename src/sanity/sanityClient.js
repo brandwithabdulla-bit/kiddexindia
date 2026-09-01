cbd5db74e1e904ba1f9a0ba98f8be9792b8d7385
abdulla n <brandwithabdulla@gmail.com>
2026-09-01 21:52:38 +0530

    ddd

--- src/sanity/sanityClient.js
+++ src/sanity/sanityClient.js
@@ -5,7 +5,7 @@ export const client = createClient({
   // TODO: Replace with your actual project ID from sanity.io
   projectId: 'uv89nln7',
   dataset: 'production',
-  useCdn: true, // set to `false` to bypass the edge cache
+  useCdn: false, // set to `false` to bypass the edge cache and see real-time updates
   apiVersion: '2024-03-01', // use current date (YYYY-MM-DD) to target the latest API version
 });
 
